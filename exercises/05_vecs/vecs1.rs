f9dc08f92a83ff90d603103419ebabc786fa4fad
ThomasMullen <thomasmullen96@gmail.com>
2024-04-02 00:39:50 +0100

    startedex05

--- exercises/05_vecs/vecs1.rs
+++ exercises/05_vecs/vecs1.rs
@@ -11,7 +11,8 @@
 
 fn array_and_vec() -> ([i32; 4], Vec<i32>) {
     let a = [10, 20, 30, 40]; // a plain array
-    let v = // TODO: declare your vector here with the macro for vectors
+    let v: Vec<i32> = Vec::new();// create an empty vector
+    let v = vec![10, 20, 30, 40];// fill vector
 
     (a, v)
 }
